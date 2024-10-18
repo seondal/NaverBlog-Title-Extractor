d1dd366c09f508cad9782800c1ff2ede571385b3
seondal <sseondal@gmail.com>
2024-10-19 02:40:59 +0900

    ✨ feat :  백준/프로그래머스 문제 링크 넣으면 블로그 만들어줌

--- extractors/boj.py
+++ extractors/boj.py
@@ -1,26 +1,10 @@
-# from selenium import webdriver
-# import time
 from requests import get
 from bs4 import BeautifulSoup
 
-def extract_boj(link):
-    # # Selenium WebDriver 설정 (Chrome 사용 예)
-    # driver = webdriver.Chrome()  # 또는 적절한 WebDriver 사용
-    # driver.get(link)
-
-    # # 페이지가 로드될 때까지 잠시 대기
-    # time.sleep(3)
-
-    # # 페이지 소스 가져오기
-    # html = driver.page_source
-    # # BeautifulSoup으로 파싱
-    # soup = BeautifulSoup(html, 'html.parser')
-
-    
+def extract_boj(link): 
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}
     response = get(f"{link}", headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
-    print(response.text)
     
     result = {}
 
@@ -42,7 +26,6 @@ def extract_boj(link):
     if problem_div:
         p_tags = problem_div.find_all("p")  # <p> 요소들을 리스트로 가져옴
         contents = [p.get_text(strip=True) for p in p_tags] # 각 <p> 요소의 텍스트 추출
-        result['contents'] = contents
+        result['content'] = contents
 
-    print(result)
     return result
